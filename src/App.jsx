3fbc063a95d777a8fa177b7a4e1608c7894b2db5
IngNex <ingnexmaicol14@gmail.com>
2022-09-05 19:13:50 -0500

    create global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <>
       <h1>React Website using Styled Components</h1>
-    </div>
+    </>
   )
 }
 

8e292ba9604ce11d1257403b86891b45f08fab57
IngNex <ingnexmaicol14@gmail.com>
2022-09-06 19:01:41 -0500

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:IngNex/react-website-basicOne.git main:gh-pages
+# git push -f git@github.com:ingnex/react-website-basicOne.git main:gh-pages
 
 cd -
 
